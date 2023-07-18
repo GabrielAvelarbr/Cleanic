8974447ee0622c11b11f92fe90cbfa3397157fc8
GabrielAvelarbr <117688731+GabrielAvelarbr@users.noreply.github.com>
2023-07-18 14:51:34 -0300

    Delete Source/Backend directory

--- Source/Backend/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASSWORD', '');
-    define('BASE', 'cleanic');
-
-    $conn = new mysqli(HOST, USER, PASSWORD, BASE);
