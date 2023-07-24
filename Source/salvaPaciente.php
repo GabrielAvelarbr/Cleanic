d285bfeffecf03cdef33e61a987f1091dd632a52
GabrielAvelarbr <117688731+GabrielAvelarbr@users.noreply.github.com>
2023-07-24 20:21:29 -0300

    Atualização salvaPaciente.php

--- Source/salvaPaciente.php
+++ Source/salvaPaciente.php
@@ -2,27 +2,31 @@
     include("config.php");
     switch ($_REQUEST["acao"]) {
         case 'cadastrar':
-            $nomePaciente = $_POST["nomePaciente"];
-            $generoPaciente = $_POST["generoPaciente"];
-            $dataDeNascimentoPaciente = $_POST["dataDeNascimentoPaciente"];
-            $emailPaciente = $_POST["emailPaciente"];
-            $telefonePaciente = $_POST["telefonePaciente"];
+            try {
+                $nomePaciente = $_POST["nomePaciente"];
+                $generoPaciente = $_POST["generoPaciente"];
+                $dataDeNascimentoPaciente = $_POST["dataDeNascimentoPaciente"];
+                $emailPaciente = $_POST["emailPaciente"];
+                $telefonePaciente = $_POST["telefonePaciente"];
 
-            $sql = "INSERT INTO paciente (nomePaciente, 
-            generoPaciente, dataDeNascimentoPaciente, emailPaciente,telefonePaciente) 
-            VALUES ('{$nomePaciente}', '{$generoPaciente}', '{$dataDeNascimentoPaciente}',
-            '{$emailPaciente}', '{$telefonePaciente}')";
+                $sql = "INSERT INTO paciente (nomePaciente, 
+                generoPaciente, dataDeNascimentoPaciente, emailPaciente,telefonePaciente) 
+                VALUES ('{$nomePaciente}', '{$generoPaciente}', '{$dataDeNascimentoPaciente}',
+                '{$emailPaciente}', '{$telefonePaciente}')";
 
-            $resultado = $conn->query($sql);
+                $resultado = $conn->query($sql);
 
-            if($resultado==true){
-                echo "<script>alert('Paciente cadastrado com sucesso');</script>";
+                if ($resultado) {
+                    echo "<script>alert('Paciente alterado com sucesso');</script>";
+                    echo "<script>location.href='todosOsPacientes.php';</script>";
+                } else {
+                    throw new Exception("Não foi possível atualizar esse Paciente. " . $conn->error);
+                }
+            } catch (Exception $e) {
+                echo "<script>alert('Erro ao atualizar o Paciente');</script>";
                 echo "<script>location.href='todosOsPacientes.php';</script>";
             }
-            else {
-                echo "<script>alert('Não foi possível cadastrar o paciente');</script>";
-                echo "<script>location.href='todosOsPacientes.php';</script>";
-            }
+            break;
             
             break;
     }
