d05591632f774d2895f56cc9663a71d2690828bf
GabrielAvelarbr <117688731+GabrielAvelarbr@users.noreply.github.com>
2023-07-24 20:18:35 -0300

    Atualização salvaConsulta.php

--- Source/salvaConsulta.php
+++ Source/salvaConsulta.php
@@ -2,74 +2,78 @@
     include("config.php");
     switch ($_REQUEST["acao"]) {
         case 'cadastrar':
-            $idMedico = $_POST["idMedico"];
-            $idPaciente = $_POST["idPaciente"];
-            $idClinica = $_POST["idClinica"];
-            $dataConsulta = $_POST["dataConsulta"];
-            $horarioConsulta = $_POST["horarioConsulta"];
-
-            $sql = "INSERT INTO consulta (idMedico, 
-            idPaciente, idClinica,dataConsulta, horarioConsulta) 
-            VALUES ('{$idMedico}', '{$idPaciente}', 
-            '{$idClinica}', '{$dataConsulta}' , '{$horarioConsulta}')";
-
-            $resultado = $conn->query($sql);
-
-            if($resultado==true){
-                echo "<script>alert('Consulta cadastrada com sucesso');</script>";
+            try {
+                $idMedico = $_POST["idMedico"];
+                $idPaciente = $_POST["idPaciente"];
+                $idClinica = $_POST["idClinica"];
+                $dataConsulta = $_POST["dataConsulta"];
+                $horarioConsulta = $_POST["horarioConsulta"];
+    
+                $sql = "INSERT INTO consulta (idMedico, 
+                idPaciente, idClinica,dataConsulta, horarioConsulta) 
+                VALUES ('{$idMedico}', '{$idPaciente}', 
+                '{$idClinica}', '{$dataConsulta}' , '{$horarioConsulta}')";
+    
+                $resultado = $conn->query($sql);
+                if ($resultado) {
+                    echo "<script>alert('Consulta excluída com sucesso');</script>";
+                    echo "<script>location.href='todasAsConsultas.php';</script>";
+                } else {
+                    throw new Exception("Não foi possível excluir essa Consulta. " . $conn->error);
+                }
+            } catch (Exception $e) {
+                echo "<script>alert('Erro ao excluir a Consulta');</script>";
                 echo "<script>location.href='todasAsConsultas.php';</script>";
             }
-            else {
-                echo "<script>alert('Não foi possível cadastrar a consulta');</script>";
-                echo "<script>location.href='todasAsConsultas.php';</script>";
-            }
-            
             break;
 
         case 'editar' :
-            $idMedico = $_POST["idMedico"];
-            $idPaciente = $_POST["idPaciente"];
-            $idClinica = $_POST["idClinica"];
-            $dataConsulta = $_POST["dataConsulta"];
-            $horarioConsulta = $_POST["horarioConsulta"];
-            
-            $sql = "UPDATE consulta SET
-                        idMedico = '{$idMedico}',
-                        idPaciente = '{$idPaciente}',
-                        idClinica = '{$idClinica}',
-                        dataConsulta = '{$dataConsulta}',
-                        horarioConsulta = '{$horarioConsulta}'
-                    WHERE
-                        idConsulta=" .$_REQUEST["idConsulta"];
-            
+            try {
+                $idMedico = $_POST["idMedico"];
+                $idPaciente = $_POST["idPaciente"];
+                $idClinica = $_POST["idClinica"];
+                $dataConsulta = $_POST["dataConsulta"];
+                $horarioConsulta = $_POST["horarioConsulta"];
+                
+                $sql = "UPDATE consulta SET
+                            idMedico = '{$idMedico}',
+                            idPaciente = '{$idPaciente}',
+                            idClinica = '{$idClinica}',
+                            dataConsulta = '{$dataConsulta}',
+                            horarioConsulta = '{$horarioConsulta}'
+                        WHERE
+                            idConsulta=" .$_REQUEST["idConsulta"];
+  
+                $resultado = $conn->query($sql);
 
-            $resultado = $conn->query($sql);
-
-            if($resultado==true){
-                echo "<script>alert('Dados da consulta alterados com sucesso');</script>";
-                echo "<script>location.href='todasAsconsultas.php';</script>";
-            }
-            else {
-                echo "<script>alert('Não foi possível modificar os dados desse consulta');</script>";
+                if ($resultado) {
+                    echo "<script>alert('Consulta excluída com sucesso');</script>";
+                    echo "<script>location.href='todasAsConsultas.php';</script>";
+                } else {
+                    throw new Exception("Não foi possível excluir essa Consulta. " . $conn->error);
+                }
+            } catch (Exception $e) {
+                echo "<script>alert('Erro ao excluir a Consulta');</script>";
                 echo "<script>location.href='todasAsConsultas.php';</script>";
             }
-        
             break;
 
         case 'excluir' :
-            $sql = "DELETE FROM consulta WHERE idConsulta=" .$_REQUEST["idConsulta"];
+    
+            try {
+                $sql = "DELETE FROM consulta WHERE idConsulta=" .$_REQUEST["idConsulta"];
+                $resultado = $conn->query($sql);
 
-            $resultado = $conn->query($sql);
-
-            if($resultado==true){
-                echo "<script>alert('Consulta excluída com sucesso');</script>";
-                echo "<script>location.href='todasAsConsultas.php';</script>";
-            }
-            else {
-                echo "<script>alert('Não foi possível excluir essa consulta');</script>";
+                if ($resultado) {
+                    echo "<script>alert('Consulta excluída com sucesso');</script>";
+                    echo "<script>location.href='todasAsConsultas.php';</script>";
+                } else {
+                    throw new Exception("Não foi possível excluir essa Consulta. " . $conn->error);
+                }
+            } catch (Exception $e) {
+                echo "<script>alert('Erro ao excluir a Consulta');</script>";
                 echo "<script>location.href='todasAsConsultas.php';</script>";
             }
             break;
     }
-    
 ?>
